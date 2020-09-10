86e4cc42e71e2be5d0c15a418ce97dae6b5bf6ca
nadav-galili <67638018+nadav-galili@users.noreply.github.com>
2020-09-10 10:16:04 +0300

    fixed about.html

--- main.js
+++ main.js
@@ -8,10 +8,9 @@ let x = setInterval(function () {
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("counter").innerHTML = days + "D " + hours + "H " +
-        minutes + "M " + seconds + "s ";
+    document.getElementById('counter').innerHTML = `${days}D ${hours}H ${minutes}M ${seconds}s `;
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("counter").innerHTML = "Welcome To Mardi Gras 2021";
+        document.getElementById('counter').innerHTML = "Welcome To Mardi Gras 2021";
     }
 }, 1000);
